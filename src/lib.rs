460f82502be9565b915529ff3c7bdee3c21fb7f4
Alexey Voznyuk <me@swizard.info>
2016-05-10 18:13:09 +0300

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+
+struct Error {
+}
+
+struct Algorithm {
+    
+}
+
+impl Algorithm {
+    fn new() -> Algorithm {
+    }
+
+    fn run() -> Result<(), Error> {
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
