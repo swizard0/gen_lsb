a99b8571c9b8289a3005c8b9443597a68ae69f47
Alexey Voznyuk <me@swizard.info>
2016-05-12 16:26:14 +0300

    slaves processing refactored + `PM` and `IM` cache for slaves

--- src/lib.rs
+++ src/lib.rs
@@ -42,29 +42,39 @@ pub enum Error<EL> where EL: ErrorsLayout {
     Population(EL::PE),
     PopulationManager(EL::PME),
     PopulationJobs(EL::PJE),
+    UnexpectedMasterCommand,
+    UnexpectedSlaveReply,
     UnexpectedEmptyPopulation,
     Several(Vec<Error<EL>>),
 }
 
 enum Command<AL> where AL: AlgorithmLayout {
-    PopulationInitialize(Arc<AL::PM>, Arc<AtomicUsize>),
-    Stop,
+    SlaveStart(Arc<AL::PM>),
+    PopulationInitialize(Arc<AtomicUsize>),
+    SlaveStop,
+    SlaveQuit,
 }
 
 impl<AL> Clone for Command<AL> where AL: AlgorithmLayout {
     fn clone(&self) -> Self {
         match *self {
-            Command::PopulationInitialize(ref population_manager, ref sync_counter) =>
-                Command::PopulationInitialize(population_manager.clone(), sync_counter.clone()),
-            Command::Stop =>
-                Command::Stop,
+            Command::SlaveStart(ref population_manager) =>
+                Command::SlaveStart(population_manager.clone()),
+            Command::PopulationInitialize(ref sync_counter) =>
+                Command::PopulationInitialize(sync_counter.clone()),
+            Command::SlaveStop =>
+                Command::SlaveStop,
+            Command::SlaveQuit =>
+                Command::SlaveQuit,
         }
     }
 }
 
 enum Report<AL> where AL: AlgorithmLayout {
+    SlaveStarted,
     PopulationInitialized(AL::P),
-    StopAck,
+    SlaveStopped,
+    SlaveQuitted,
 }
 
 impl<AL> Report<AL> where AL: AlgorithmLayout {
@@ -72,6 +82,16 @@ impl<AL> Report<AL> where AL: AlgorithmLayout {
         match self {
             Report::PopulationInitialized(population) => population,
             _ => unreachable!(),
+        }
+    }
+
+    fn same_type(&self, other: &Report<AL>) -> bool {
+        match (self, other) {
+            (&Report::SlaveStarted, &Report::SlaveStarted) => true,
+            (&Report::PopulationInitialized(..), &Report::PopulationInitialized(..)) => true,
+            (&Report::SlaveStopped, &Report::SlaveStopped) => true,
+            (&Report::SlaveQuitted, &Report::SlaveQuitted) => true,
+            _ => false,
         }
     }
 }
@@ -89,7 +109,7 @@ impl<AL> Slave<AL> where AL: AlgorithmLayout {
         let (slave_tx, master_rx) = channel();
         let maybe_thread = thread::Builder::new()
             .name(format!("gen_lsb slave #{}", slave_id))
-            .spawn(move || slave_loop(slave_rx, slave_tx));
+            .spawn(move || slave_idle(slave_rx, slave_tx));
         Ok(Slave {
             thread: Some(try!(maybe_thread)),
             tx: master_tx,
@@ -102,10 +122,10 @@ impl<AL> Slave<AL> where AL: AlgorithmLayout {
 impl<AL> Drop for Slave<AL> where AL: AlgorithmLayout {
     fn drop(&mut self) {
         if let Some(thread) = self.thread.take() {
-            self.tx.send(Command::Stop).unwrap();
+            self.tx.send(Command::SlaveQuit).unwrap();
             loop {
                 match self.rx.recv().unwrap() {
-                    Ok(Report::StopAck) => {
+                    Ok(Report::SlaveQuitted) => {
                         thread.join().unwrap();
                         break;
                     },
@@ -119,17 +139,22 @@ impl<AL> Drop for Slave<AL> where AL: AlgorithmLayout {
     }
 }
 
-fn slave_loop<AL>(rx: Receiver<Command<AL>>, tx: Sender<Result<Report<AL>, Error<AL::EL>>>) where AL: AlgorithmLayout {
+fn slave_idle<AL>(rx: Receiver<Command<AL>>, tx: Sender<Result<Report<AL>, Error<AL::EL>>>) where AL: AlgorithmLayout {
     loop {
         match rx.recv().unwrap() {
-            Command::PopulationInitialize(population_manager, sync_counter) => {
-                let result = slave_population_init::<AL>(population_manager, sync_counter);
-                tx.send(result.map(|r| Report::PopulationInitialized(r))).unwrap();
-            },
-            Command::Stop => {
-                tx.send(Ok(Report::StopAck)).unwrap();
+            Command::SlaveStart(population_manager) =>
+                match population_manager.make_individual_manager() {
+                    Ok(individual_manager) =>
+                        slave_loop(&*population_manager, individual_manager, &rx, &tx),
+                    Err(err) =>
+                        tx.send(Err(Error::PopulationManager(err))).unwrap(),
+                },
+            Command::SlaveQuit => {
+                tx.send(Ok(Report::SlaveQuitted)).unwrap();
                 break;
             },
+            _ =>
+                tx.send(Err(Error::UnexpectedMasterCommand)).unwrap(),
         }
     }
 }
@@ -144,12 +169,27 @@ impl Iterator for SyncIter {
     }
 }
 
-fn slave_population_init<AL>(population_manager: Arc<AL::PM>, sync_counter: Arc<AtomicUsize>) ->
-    Result<AL::P, Error<AL::EL>> where AL: AlgorithmLayout
+fn slave_loop<AL>(population_manager: &AL::PM,
+                  mut individual_manager: AL::IM,
+                  rx: &Receiver<Command<AL>>,
+                  tx: &Sender<Result<Report<AL>, Error<AL::EL>>>) where AL: AlgorithmLayout
 {
-    let mut individual_manager =
-        try!(population_manager.make_individual_manager().map_err(|e| Error::PopulationManager(e)));
-    population_manager.jobs().init(&mut individual_manager, SyncIter(sync_counter)).map_err(|e| Error::PopulationJobs(e))
+    let population_jobs = population_manager.jobs();
+    loop {
+        match rx.recv().unwrap() {
+            Command::PopulationInitialize(sync_counter) =>
+                tx.send(match population_jobs.init(&mut individual_manager, SyncIter(sync_counter)) {
+                    Ok(population) => Ok(Report::PopulationInitialized(population)),
+                    Err(err) => Err(Error::PopulationJobs(err)),
+                }).unwrap(),
+            Command::SlaveStop => {
+                tx.send(Ok(Report::SlaveStopped)).unwrap();
+                break;
+            },
+            Command::SlaveStart(..) | Command::SlaveQuit =>
+                tx.send(Err(Error::UnexpectedMasterCommand)).unwrap(),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -172,15 +212,24 @@ impl<AL> Algorithm<AL> where AL: AlgorithmLayout {
     }
 
     pub fn run(&mut self) -> Result<RunResult<AL::I>, Error<AL::EL>> {
+        let start_cmd = Command::SlaveStart(self.population_manager.clone());
+        try!(self.spread_sync_expect(start_cmd, Report::SlaveStarted));
+        match (self.run_with_guard(), self.spread_sync_expect(Command::SlaveStop, Report::SlaveStopped)) {
+            (Ok(r), Ok(())) => Ok(r),
+            (Ok(..), Err(err)) => Err(err),
+            (Err(err), Ok(())) => Err(err),
+            (Err(ea), Err(eb)) => Err(Error::Several(vec![ea, eb])),
+        }
+    }
+
+    fn run_with_guard(&mut self) -> Result<RunResult<AL::I>, Error<AL::EL>> {
         let _population = self.population_initialize();
 
         Ok(RunResult::PopLimitExceeded)
     }
 
     pub fn population_initialize(&mut self) -> Result<AL::P, Error<AL::EL>> {
-        let init_command =
-            Command::PopulationInitialize(self.population_manager.clone(), Arc::new(AtomicUsize::new(0)));
-        try!(self.spread_sync(init_command));
+        try!(self.spread_sync(Command::PopulationInitialize(Arc::new(AtomicUsize::new(0)))));
         let maybe_population =
             Set::merge_many(self.slaves.iter_mut().flat_map(|s| s.last_report.take()).map(|r| r.population_results()));
         try!(maybe_population.map_err(|e| Error::Population(e))).ok_or(Error::UnexpectedEmptyPopulation)
@@ -201,9 +250,23 @@ impl<AL> Algorithm<AL> where AL: AlgorithmLayout {
         }
         if errors.is_empty() {
             Ok(())
+        } else if errors.len() == 1 {
+            Err(errors.pop().unwrap())
         } else {
             Err(Error::Several(errors))
         }
+    }
+
+    fn spread_sync_expect(&mut self, cmd: Command<AL>, rep: Report<AL>) -> Result<(), Error<AL::EL>> {
+        try!(self.spread_sync(cmd));
+        for slave_reply in self.slaves.iter_mut().map(|s| s.last_report.take()) {
+            if let Some(true) = slave_reply.map(|r| r.same_type(&rep)) {
+                continue;
+            } else {
+                return Err(Error::UnexpectedSlaveReply);
+            }
+        }
+        Ok(())
     }
 }
 
