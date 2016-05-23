359ac19fb9d18fd4d3c6cb7c2d5567e08e393a74
Alexey Voznyuk <me@swizard.info>
2016-05-23 16:48:04 +0300

    basic implementation skeleton

--- src/pop/fit/standard.rs
+++ src/pop/fit/standard.rs
@@ -35,3 +35,44 @@ pub trait Policy {
     type FitsME: Send + 'static;
     type FitsM: SetManager<S = Self::Fits, E = Self::FitsME>;
 }
+
+pub struct StandardPopulationFit<P> where P: Policy {
+    _marker: PhantomData<P>,
+}
+
+impl<P> StandardPopulationFit<P> where P: Policy {
+    pub fn new() -> StandardPopulationFit<P> {
+        StandardPopulationFit {
+            _marker: PhantomData,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum FitnessError<PE, FE, FME, IME> {
+    Population(PE),
+    FitsSet(FE),
+    FitsSetManager(FME),
+    IndividualManager(IME),
+}
+
+#[derive(Debug)]
+pub enum Error<ExecE, PopE, FitsE, FitsME, IndivME> {
+    NoOutputFitnessValues,
+    Executor(ExecutorJobError<ExecE, JobExecuteError<FitnessError<PopE, FitsE, FitsME, IndivME>, union::Error<FitsE, FitsME>>>),
+}
+
+pub type ErrorP<P> where P: Policy = Error<<P::Exec as Executor>::E, P::PopE, P::FitsE, P::FitsME, P::IndivME>;
+
+impl<P> PopulationFit for StandardPopulationFit<P> where P: Policy {
+    type Exec = P::Exec;
+    type Indiv = P::Indiv;
+    type Pop = P::Pop;
+    type Fit = P::Fit;
+    type Fits = P::Fits;
+    type Err = ErrorP<P>;
+
+    fn fit(&self, _population: &Self::Pop, _exec: &mut Self::Exec) -> Result<Self::Fits, Self::Err> {
+        Err(Error::NoOutputFitnessValues::<<Self::Exec as Executor>::E, P::PopE, P::FitsE, P::FitsME, P::IndivME>)
+    }
+}
